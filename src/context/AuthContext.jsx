9d3bbe887e3423f681e3b53e30cf4f4751b3169e
NIkhil <nikhilghinamine@gmail.com>
2026-04-25 22:28:36 +0530

    Updated the backend URL

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -16,7 +16,7 @@ export const AuthProvider = ({ children }) => {
 
   const api = useRef(
     axios.create({
-      baseURL: "https://roomease-ua99.onrender.com",
+      baseURL: import.meta.env.VITE_API_URL,
       withCredentials: true,
     }),
   ).current;
